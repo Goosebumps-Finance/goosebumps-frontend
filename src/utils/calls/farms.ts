3bf6d97d593fcaffd046fed5628361bc2a729660
CryptoSnowPrince <topdirector2017@gmail.com>
2022-07-22 14:23:12 +0200

    update stake and unstake and harvest

--- src/utils/calls/farms.ts
+++ src/utils/calls/farms.ts
@@ -25,7 +25,8 @@ export const stakeFarm = async (stakingContract, amount, decimals?) => {
   const gasPrice = getGasPrice()
   const value = new BigNumber(amount).times(decimals ? BIG_TEN.pow(decimals) : DEFAULT_TOKEN_DECIMAL).toString()
 
-  const tx = await stakingContract.stake(value, { ...options, gasPrice })
+  // const tx = await stakingContract.stake(value, { ...options, gasPrice })
+  const tx = await stakingContract.stake(value, { gasPrice })
   const receipt = await tx.wait()
   return receipt.status
 }
@@ -47,7 +48,8 @@ export const unstakeFarmOld = async (masterChefContract, pid, amount) => {
 export const unstakeFarm = async (stakingContract, amount, decimals?) => {
   const gasPrice = getGasPrice()
   const value = new BigNumber(amount).times(decimals ? BIG_TEN.pow(decimals) : DEFAULT_TOKEN_DECIMAL).toString()
-  const tx = await stakingContract.unstake(value, { ...options, gasPrice })
+  // const tx = await stakingContract.unstake(value, { ...options, gasPrice })
+  const tx = await stakingContract.unstake(value, { gasPrice })
   const receipt = await tx.wait()
   return receipt.status
 }
@@ -67,7 +69,8 @@ export const harvestFarmOld = async (masterChefContract, pid) => {
 
 export const harvestFarm = async (stakingContract) => {
   const gasPrice = getGasPrice()
-  const tx = await stakingContract.withdrawRewards({ ...options, gasPrice })
+  // const tx = await stakingContract.withdrawRewards({ ...options, gasPrice })
+  const tx = await stakingContract.withdrawRewards({ gasPrice })
   const receipt = await tx.wait()
   return receipt.status
 }

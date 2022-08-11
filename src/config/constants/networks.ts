f9f120a7278265ab6e3a579714e0153a916f8c8d
CryptoSnowPrince <topdirector2017@gmail.com>
2022-08-11 17:30:28 +0200

    replace rpc again

--- src/config/constants/networks.ts
+++ src/config/constants/networks.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@goosebumps/sdk'
 
 const NETWORK_URLS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: 'https://cryptosnowprince.com:2083/rpc',
-  [ChainId.TESTNET]: 'https://cryptosnowprince.com:2083/trpc'
+  [ChainId.TESTNET]: 'https://data-seed-prebsc-2-s2.binance.org:8545'
 }
 
 export default NETWORK_URLS

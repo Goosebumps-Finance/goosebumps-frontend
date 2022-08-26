e54a73d470726414377e4c46291caf712b44d499
CryptoSnowPrince <topdirector2017@gmail.com>
2022-08-27 01:25:51 +0200

    update stake and pools estimateGas

--- src/utils/calls/farms.ts
+++ src/utils/calls/farms.ts
@@ -1,13 +1,15 @@
 import BigNumber from 'bignumber.js'
+import { Contract } from '@ethersproject/contracts'
 import { DEFAULT_GAS_LIMIT, DEFAULT_TOKEN_DECIMAL } from 'config'
 import { BIG_TEN } from 'utils/bigNumber'
 import getGasPrice from 'utils/getGasPrice'
+import { estimateGas } from './estimateGas'
 
 const options = {
   gasLimit: DEFAULT_GAS_LIMIT * 1.5,
 }
 
-export const stakeFarmOld = async (masterChefContract, pid, amount) => {
+export const stakeFarmOld = async (masterChefContract: Contract, pid: number, amount: string) => {
   const gasPrice = getGasPrice()
   const value = new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString()
   if (pid === 0) {
@@ -21,17 +23,19 @@ export const stakeFarmOld = async (masterChefContract, pid, amount) => {
   return receipt.status
 }
 
-export const stakeFarm = async (stakingContract, amount, decimals?) => {
+export const stakeFarm = async (stakingContract: Contract, amount: string, decimals?: number) => {
   const gasPrice = getGasPrice()
   const value = new BigNumber(amount).times(decimals ? BIG_TEN.pow(decimals) : DEFAULT_TOKEN_DECIMAL).toString()
 
-  const tx = await stakingContract.stake(value, { ...options, gasPrice })
+  const estimatedGas = estimateGas(stakingContract, 'stake', [value], 2000)
+  const tx = await stakingContract.stake(value, { gasLimit: estimatedGas, gasPrice })
+  // const tx = await stakingContract.stake(value, { ...options, gasPrice })
   // const tx = await stakingContract.stake(value, { gasPrice })
   const receipt = await tx.wait()
   return receipt.status
 }
 
-export const unstakeFarmOld = async (masterChefContract, pid, amount) => {
+export const unstakeFarmOld = async (masterChefContract: Contract, pid: number, amount: string) => {
   const gasPrice = getGasPrice()
   const value = new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString()
   if (pid === 0) {
@@ -45,16 +49,19 @@ export const unstakeFarmOld = async (masterChefContract, pid, amount) => {
   return receipt.status
 }
 
-export const unstakeFarm = async (stakingContract, amount, decimals?) => {
+export const unstakeFarm = async (stakingContract: Contract, amount: string, decimals?: number) => {
   const gasPrice = getGasPrice()
   const value = new BigNumber(amount).times(decimals ? BIG_TEN.pow(decimals) : DEFAULT_TOKEN_DECIMAL).toString()
-  const tx = await stakingContract.unstake(value, { ...options, gasPrice })
+
+  const estimatedGas = estimateGas(stakingContract, 'unstake', [value], 2000)
+  const tx = await stakingContract.unstake(value, { gasLimit: estimatedGas, gasPrice })
+  // const tx = await stakingContract.unstake(value, { ...options, gasPrice })
   // const tx = await stakingContract.unstake(value, { gasPrice })
   const receipt = await tx.wait()
   return receipt.status
 }
 
-export const harvestFarmOld = async (masterChefContract, pid) => {
+export const harvestFarmOld = async (masterChefContract: Contract, pid: number) => {
   const gasPrice = getGasPrice()
   if (pid === 0) {
     const tx = await masterChefContract.leaveStaking('0', { ...options, gasPrice })
@@ -67,9 +74,12 @@ export const harvestFarmOld = async (masterChefContract, pid) => {
   return receipt.status
 }
 
-export const harvestFarm = async (stakingContract) => {
+export const harvestFarm = async (stakingContract: Contract) => {
   const gasPrice = getGasPrice()
-  const tx = await stakingContract.withdrawRewards({ ...options, gasPrice })
+  
+  const estimatedGas = estimateGas(stakingContract, 'withdrawRewards', [], 2000)
+  const tx = await stakingContract.withdrawRewards({ gasLimit: estimatedGas, gasPrice })
+  // const tx = await stakingContract.withdrawRewards({ ...options, gasPrice })
   // const tx = await stakingContract.withdrawRewards({ gasPrice })
   const receipt = await tx.wait()
   return receipt.status
